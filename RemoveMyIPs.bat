df214f3fbfa7b7ddd3c206240c49e1cca0023b75
Rob Randall <rob.randall1@gmail.com>
2021-05-28 11:03:35 +0100

    add sg to calls

--- RemoveMyIPs.bat
+++ RemoveMyIPs.bat
@@ -4,10 +4,9 @@ REM Can use this when logging off from windows to be extra secure.
 set DIR=%~pd0
 
 REM SSH
-CALL %DIR%\UpdateIP.bat -p 22 -r
+CALL %DIR%\UpdateIP.bat -sg sg-0c794359dc78016af -p 22 -r
 
 REM Mongo Express
-CALL %DIR%\UpdateIP.bat -p 8081 -r
+CALL %DIR%\UpdateIP.bat -sg sg-0c794359dc78016af -p 8081 -r
 
-REM Docker Django
-CALL %DIR%\UpdateIP.bat -p 8000 -r
+REM Docker Django1
--- UpdateMyIPs.bat
+++ UpdateMyIPs.bat
@@ -4,10 +4,19 @@ REM This is a smaple wrapper for UpdateIP.bat
 set DIR=%~pd0
 
 REM SSH
-CALL %DIR%\UpdateIP.bat -p 22
+CALL %DIR%\UpdateIP.bat -sg sg-0c794359dc78016af 
 
 REM Mongo Express
-CALL %DIR%\UpdateIP.bat -p 8081
+CALL %DIR%\UpdateIP.bat -p 8081 -sg sg-0c794359dc78016af 
 
 REM Docker Django
-CALL %DIR%\UpdateIP.bat -p 8000
+REM CALL %DIR%\UpdateIP.bat -p 8888 -sg sg-0c794359dc78016af 
+
+REM Docker Redis
+CALL %DIR%\UpdateIP.bat -p 6379 -sg sg-0c794359dc78016af 
+
+REM Django protected pages
+CALL %DIR%\UpdateIP.bat -p 8443 -sg sg-0c794359dc78016af 
+
+REM Access to IDB
+CALL %DIR%\UpdateIP.bat -p 443 -sg sg-0dca9eb1846b7a219
